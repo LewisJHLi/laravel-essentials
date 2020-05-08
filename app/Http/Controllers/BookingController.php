027b9b6cf2d165f437a1007a7bef0420faf50320
Lewis Li <>
2020-05-08 17:59:10 +1000

    new saving method using model

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -49,17 +49,24 @@ class BookingController extends Controller
     public function store(Request $request)
     {
         // dd($request->all());
-        $id = DB::table('bookings')->insertGetId([
-            'room_id' => $request->input('room_id'),
-            'start' => $request->input('start'),
-            'end' => $request->input('end'),
-            'is_reservation' => $request->input('is_reservation', false),
-            'is_paid' => $request->input('is_paid', false),
-            'notes' => $request->input('notes'),
-        ]);
+        // $id = DB::table('bookings')->insertGetId([
+        //     'room_id' => $request->input('room_id'),
+        //     'start' => $request->input('start'),
+        //     'end' => $request->input('end'),
+        //     'is_reservation' => $request->input('is_reservation', false),
+        //     'is_paid' => $request->input('is_paid', false),
+        //     'notes' => $request->input('notes'),
+        // ]);
+        
+        // DB::table('bookings_users')->insert([
+        //     'booking_id' => $id,
+        //     'user_id' => $request->input('user_id'),
+        // ]);
+
+        $booking = Booking::create($request->input());
 
         DB::table('bookings_users')->insert([
-            'booking_id' => $id,
+            'booking_id' => $booking->id,
             'user_id' => $request->input('user_id'),
         ]);
 
