2b33dd39d34ca32d2c46a6afe81901558fb7be60
Lewis Li <>
2020-05-08 14:46:43 +1000

    the first controller make:controller ShowRoomsController --invokable

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/test', function(){ return 'Good bye'; });
+
+Route::get('/rooms', 'ShowRoomsController');
